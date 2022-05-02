a57e058cfe75740b0fea774b2c2e6f5555241c2d
ppcamp <p.augustocampos@gmail.com>
2022-05-02 10:44:04 -0300

    feat: grid

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -18,29 +18,36 @@
 
   .container {
     display: grid;
-    grid-template-columns: 3fr 1fr;
+    grid-template-columns: 3fr 1fr 1fr;
     grid-template-rows: 20vh 70vh 10vh;
-    grid-template-areas: "header header" "main aside" "footer footer";
+    grid-template-areas:
+      "header header header"
+      "main main aside"
+      "footer footer footer";
     gap: 1em 1em;
   }
 
   header {
     background-color: rgb(9, 255, 0);
     grid-area: header;
+    /* grid-column: 1/4; */
   }
 
   main {
     background-color: aqua;
     grid-area: main;
+    /* grid-column: 1/2; */
   }
 
   footer {
     background-color: red;
     grid-area: footer;
+    /* grid-column: 1/4; */
   }
 
   aside {
     background-color: yellow;
     grid-area: aside;
+    /* grid-column: 2/4; */
   }
 </style>
