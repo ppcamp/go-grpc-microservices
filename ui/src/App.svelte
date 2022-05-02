40da654d7717b7fde59534af1137ab354fc538bf
ppcamp <p.augustocampos@gmail.com>
2022-05-02 10:24:19 -0300

    feat: initialize ui

---
+++ ui/src/App.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+</script>
+
+<div class="container">
+  <header>Hader</header>
+  <main>Main</main>
+  <aside>Aside</aside>
+  <footer>Footer</footer>
+</div>
+
+<style>
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background: #fafafa;
+  }
+
+  .container {
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    grid-template-rows: 20vh 70vh 10vh;
+    grid-template-areas: "header header" "main aside" "footer footer";
+    gap: 1em 1em;
+  }
+
+  header {
+    background-color: rgb(9, 255, 0);
+    grid-area: header;
+  }
+
+  main {
+    background-color: aqua;
+    grid-area: main;
+  }
+
+  footer {
+    background-color: red;
+    grid-area: footer;
+  }
+
+  aside {
+    background-color: yellow;
+    grid-area: aside;
+  }
+</style>
